1fe4a04b2cda59bb4cd833630dd372475f5680ca
din-islam-rajin <din.islam.rajin572@gmail.com>
2023-11-25 15:04:49 +0600

    Change the listen log

--- src/server.ts
+++ src/server.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
     await mongoose.connect(config.database_url as string);
 
     app.listen(config.port, () => {
-      console.log(`app is listening on port ${config.port}`);
+      console.log(`App is listening on port ${config.port}`);
     });
   } catch (error) {
     console.log(error);
